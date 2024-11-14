8609aca42c084ba8f9f7a0d839ba14cf683b0353
HaseebQamar69 <haseebqamar69@gmail.com>
2024-11-14 16:31:42 +0500

    code

--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -2,14 +2,16 @@ import { createBrowserRouter } from 'react-router-dom';
 import DashboardLayout from '../components/DashboardLayout.jsx';
 import Logout from '../pages/Logout/index.jsx';
 import Offers from '../pages/Offers/index.jsx';
+import Login from '../pages/Auth/Login.jsx';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <DashboardLayout />, // Use DashboardLayout directly if needed as the main layout
     children: [
-      { path: '/dashboard/Offers', element: <Offers /> },
-      { path: '/dashboard/Logout', element: <Logout /> },
+      { path: "/dashboard/Offers", element: <Offers /> },
+      { path: "/dashboard/Logout", element: <Logout /> },
+      { path: "/dashboard/login", element: <Login /> },
       // Add more nested routes as needed
     ],
   },
