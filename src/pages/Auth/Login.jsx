c8e9b19bffc3506ad8c920177a0cb2440ebc3243
HaseebQamar69 <haseebqamar69@gmail.com>
2024-11-14 16:55:53 +0500

    New code

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
     <>
-      <section
-        className=" border border-red-600 bg-[#2C2C2C] h-screen w-full"
-      ></section>
+      <section className=" bg-[#2C2C2C] h-screen w-full">
+        <div className=" flex">
+          <div className=" h-screen w-full bg-no-repeat bg-contain">
+            <div className="loginimg h-screen w-full bg-no-repeat bg-contain"></div>
+          </div>
+          <div>2</div>
+        </div>
+      </section>
     </>
   );
 }
 
-export default Login
+export default Login;
--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -1,18 +1,20 @@
-import { createBrowserRouter } from 'react-router-dom';
-import DashboardLayout from '../components/DashboardLayout.jsx';
-import Logout from '../pages/Logout/index.jsx';
-import Offers from '../pages/Offers/index.jsx';
-import Login from '../pages/Auth/Login.jsx';
+import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../components/DashboardLayout.jsx";
+import Logout from "../pages/Logout/index.jsx";
+import Offers from "../pages/Offers/index.jsx";
+import Login from "../pages/Auth/Login.jsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <DashboardLayout />, // Use DashboardLayout directly if needed as the main layout
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
     children: [
       { path: "/dashboard/Offers", element: <Offers /> },
       { path: "/dashboard/Logout", element: <Logout /> },
-      { path: "/dashboard/login", element: <Login /> },
-      // Add more nested routes as needed
     ],
   },
 ]);
